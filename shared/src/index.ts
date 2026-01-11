d2f054bf89ade9dc26b4d7e9333c091898870a0f
abdullah <aabdullah.zayed0099@gmail.com>
2026-01-12 01:37:56 +0200

    add deps and ts configs

---
+++ shared/src/index.ts
@@ -0,0 +1,15 @@
+// Enums
+export * from "./enums/roles";
+export * from "./enums/order-status";
+export * from "./enums/delivery-status";
+export * from "./enums/shop-status";
+
+// Events
+export * from "./events/event-types";
+export * from "./events/base-event";
+export * from "./events/event-bus";
+
+// Utils
+export * from "./utils/response";
+export * from "./utils/errors";
+export * from "./utils/logger";
